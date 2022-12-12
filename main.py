033efb7b4b957160569a43cebed9123c15f94e90
Maverick-Hamer <maverickhamer06@gmail.com>
2022-12-12 08:59:37 -0800

    moved simulation class to main

--- main.py
+++ main.py
@@ -164,3 +164,49 @@ class WorldCupMatch(object):
                 self.winner = self.team1
             else:
                 self.winner = self.team2
+
+
+from WorldCupGroup import WorldCupGroup
+from GoHomeRounds import WorldCupKnockOut
+
+class WorldCupSim(object):
+    def __init__(self,group_names,teams,verbose):
+        self.group_names = group_names
+        self.teams = teams
+        self.groups = []
+        self.verbose = verbose
+        
+    def runsim(self):
+        # Run full World Cup Sim
+        # Put teams in groups
+        for g in self.group_names:
+            group_teams = [t for t in self.teams if t.group==g]
+            self.groups.append(WorldCupGroup(g,group_teams))
+        # Simulation group matches
+        for g in self.groups:
+            g.simulate_group_matches()
+            if self.verbose:
+                g.print_matches()
+                g.print_table()
+        # BUild knock-out stage of tournament
+        self.KnockOut = WorldCupKnockOut(self.groups)
+        # ROUND OF 16
+        self.KnockOut.Round16()
+        self.KnockOut.simulate_Round16_matches()
+        if self.verbose:        
+            self.KnockOut.print_matches(self.KnockOut.R16matches)
+        # Quarter Finals
+        self.KnockOut.QuarterFinal()
+        self.KnockOut.simulate_QF_matches()
+        if self.verbose:
+            self.KnockOut.print_matches(self.KnockOut.QFmatches)
+        # Semi Finals
+        self.KnockOut.SemiFinal()
+        self.KnockOut.simulate_SF_matches()
+        if self.verbose:        
+            self.KnockOut.print_matches(self.KnockOut.SFmatches)
+        # Final
+        self.KnockOut.Final()
+        self.KnockOut.simulate_Final()
+        if self.verbose:
+            self.KnockOut.print_matches(self.KnockOut.Final)
